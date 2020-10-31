38f916797aaa68a1d9a9ecd87579f76163848cc6
GiaKhai <khaigia11111@gmail.com>
2020-10-31 18:10:50 +0700

    UI order

---
+++ FormMainGUI/Forms/AddOrder.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FormMainGUI.Forms
+{
+    partial class AddOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddOrder));
+            this.button1 = new System.Windows.Forms.Button();
+            this.selectProduct = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.quantity = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // selectProduct
+            // 
+            resources.ApplyResources(this.selectProduct, "selectProduct");
+            this.selectProduct.ForeColor = System.Drawing.SystemColors.Window;
+            this.selectProduct.Name = "selectProduct";
+            // 
+            // search
+            // 
+            resources.ApplyResources(this.search, "search");
+            this.search.ForeColor = System.Drawing.Color.White;
+            this.search.Name = "search";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            // 
+            // quantity
+            // 
+            resources.ApplyResources(this.quantity, "quantity");
+            this.quantity.ForeColor = System.Drawing.Color.White;
+            this.quantity.Name = "quantity";
+            this.quantity.Click += new System.EventHandler(this.quantity_Click);
+            // 
+            // numericUpDown1
+            // 
+            resources.ApplyResources(this.numericUpDown1, "numericUpDown1");
+            this.numericUpDown1.Name = "numericUpDown1";
+            // 
+            // AddOrder
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.selectProduct);
+            this.Controls.Add(this.button1);
+            this.Name = "AddOrder";
+            this.Load += new System.EventHandler(this.AddOrder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label selectProduct;
+        private System.Windows.Forms.Label search;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label quantity;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
---
+++ FormMainGUI/Forms/UpdateOrder.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace FormMainGUI.Forms
+{
+    partial class UpdateOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.productId = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.Label();
+            this.quantity = new System.Windows.Forms.Label();
+            this.productName = new System.Windows.Forms.Label();
+            this.mfg = new System.Windows.Forms.Label();
+            this.exp = new System.Windows.Forms.Label();
+            this.txbID = new System.Windows.Forms.TextBox();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(126, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Update Order";
+            // 
+            // productId
+            // 
+            this.productId.AutoSize = true;
+            this.productId.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productId.ForeColor = System.Drawing.Color.White;
+            this.productId.Location = new System.Drawing.Point(48, 148);
+            this.productId.Name = "productId";
+            this.productId.Size = new System.Drawing.Size(92, 24);
+            this.productId.TabIndex = 1;
+            this.productId.Text = "Product ID";
+            // 
+            // price
+            // 
+            this.price.AutoSize = true;
+            this.price.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.price.ForeColor = System.Drawing.Color.White;
+            this.price.Location = new System.Drawing.Point(48, 318);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(49, 24);
+            this.price.TabIndex = 2;
+            this.price.Text = "Price";
+            // 
+            // quantity
+            // 
+            this.quantity.AutoSize = true;
+            this.quantity.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantity.ForeColor = System.Drawing.Color.White;
+            this.quantity.Location = new System.Drawing.Point(48, 260);
+            this.quantity.Name = "quantity";
+            this.quantity.Size = new System.Drawing.Size(75, 24);
+            this.quantity.TabIndex = 3;
+            this.quantity.Text = "Quantity";
+            // 
+            // productName
+            // 
+            this.productName.AutoSize = true;
+            this.productName.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productName.ForeColor = System.Drawing.Color.White;
+            this.productName.Location = new System.Drawing.Point(48, 203);
+            this.productName.Name = "productName";
+            this.productName.Size = new System.Drawing.Size(120, 24);
+            this.productName.TabIndex = 4;
+            this.productName.Text = "Product Name";
+            // 
+            // mfg
+            // 
+            this.mfg.AutoSize = true;
+            this.mfg.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mfg.ForeColor = System.Drawing.Color.White;
+            this.mfg.Location = new System.Drawing.Point(48, 382);
+            this.mfg.Name = "mfg";
+            this.mfg.Size = new System.Drawing.Size(47, 24);
+            this.mfg.TabIndex = 5;
+            this.mfg.Text = "MFG";
+            // 
+            // exp
+            // 
+            this.exp.AutoSize = true;
+            this.exp.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exp.ForeColor = System.Drawing.Color.White;
+            this.exp.Location = new System.Drawing.Point(48, 440);
+            this.exp.Name = "exp";
+            this.exp.Size = new System.Drawing.Size(43, 24);
+            this.exp.TabIndex = 6;
+            this.exp.Text = "EXP";
+            // 
+            // txbID
+            // 
+            this.txbID.Location = new System.Drawing.Point(185, 145);
+            this.txbID.Multiline = true;
+            this.txbID.Name = "txbID";
+            this.txbID.Size = new System.Drawing.Size(235, 27);
+            this.txbID.TabIndex = 7;
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(185, 200);
+            this.txbName.Multiline = true;
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(235, 27);
+            this.txbName.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(185, 315);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(235, 27);
+            this.textBox3.TabIndex = 9;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(185, 260);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(72, 22);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(185, 384);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(235, 22);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(185, 442);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(235, 22);
+            this.dateTimePicker2.TabIndex = 12;
+            // 
+            // UpdateOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.ClientSize = new System.Drawing.Size(494, 544);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.txbID);
+            this.Controls.Add(this.exp);
+            this.Controls.Add(this.mfg);
+            this.Controls.Add(this.productName);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.productId);
+            this.Controls.Add(this.label1);
+            this.Name = "UpdateOrder";
+            this.Text = "UpdateOrder";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label productId;
+        private System.Windows.Forms.Label price;
+        private System.Windows.Forms.Label quantity;
+        private System.Windows.Forms.Label productName;
+        private System.Windows.Forms.Label mfg;
+        private System.Windows.Forms.Label exp;
+        private System.Windows.Forms.TextBox txbID;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    }
+}
