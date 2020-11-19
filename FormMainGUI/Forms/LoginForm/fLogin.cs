b6495e39857f4fd42f04f8e8e40f28fa3b86f0db
Tai9 <159iker@gmail.com>
2020-11-19 15:05:55 +0700

    gitignore Model folder

---
+++ FormMainGUI/Forms/LoginForm/fLogin.cs
@@ -0,0 +1,61 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormMainGUI.Forms.LoginForm
+{
+    public partial class fLogin : MaterialForm
+    {
+        public fLogin()
+        {
+            InitializeComponent();
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue400, Primary.Blue500,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+            btnLogin.AutoSize = false;
+            btnCancel.AutoSize = false;
+
+            txtUsername.Size= new System.Drawing.Size(210, 40);
+            txtPassword.Size= new System.Drawing.Size(210, 40);
+
+            btnLogin.Size = new System.Drawing.Size(100, 36);
+            btnCancel.Size = new System.Drawing.Size(100, 36);
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Do you want to exit?", "Notification", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            FormMain formMain = new FormMain();
+            this.Hide();
+            formMain.ShowDialog();
+            this.Show();
+        }
+    }
+}
