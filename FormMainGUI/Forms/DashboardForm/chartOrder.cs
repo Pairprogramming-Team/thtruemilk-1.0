e13c31a059e0424c98955512d8d685fbe2c0097a
ThienHi <nguyenkimthien2603@gmail.com>
2020-12-01 15:28:42 +0700

    chart employee chart product - fix chart orders

---
+++ FormMainGUI/Forms/DashboardForm/chartOrder.cs
@@ -0,0 +1,51 @@
+﻿using FormMainGUI.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FormMainGUI.Forms.DashboardForm
+{
+    public partial class chartOrder : Form
+    {
+        public chartOrder()
+        {
+            InitializeComponent();
+            ChartOrder();
+        }
+
+        public void ChartOrder()
+        {
+            var db = DataProvider.Ins.DB;
+            var data = (db.Orders.GroupBy(d => d.DateOfOrder.Value.Month)).ToList();
+            var area = new ChartArea();
+            var chartTotal = new Chart();
+            chartTotal.ChartAreas.Add(area);
+            var series = new Series();
+            foreach (var item in data)
+            {
+                //var a = DateTime.Parse(Convert.ToString(item.DateOfOrder));
+                series.Points.AddXY(item.Key, item.Select(i => i.TotalMoney).Sum());
+            }
+            series.ChartType = SeriesChartType.Column;
+            series.Label = "#PERCENT{P0}";
+            series.Font = new Font("Arial", 8.0f, FontStyle.Bold);
+            series.Palette = ChartColorPalette.Light;
+            series.Name = "Total Money";
+            Title title = new Title("Total Money in Year");
+            title.Font = new Font("Matura MT Script Capitals", 14f, FontStyle.Bold);
+            chartTotal.Titles.Add(title);
+
+            //series["PieLabelStyle"] = "Outside";
+            chartTotal.Dock = DockStyle.Fill;
+            chartTotal.Series.Add(series);
+            panelOrder.Controls.Add(chartTotal);
+        }
+    }
+}
---
+++ FormMainGUI/Forms/DashboardForm/chartProduct.cs
@@ -0,0 +1,48 @@
+﻿using FormMainGUI.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FormMainGUI.Forms.DashboardForm
+{
+    public partial class chartProduct : Form
+    {
+        public chartProduct()
+        {
+            InitializeComponent();
+            ChartProduct();
+        }
+
+        private void ChartProduct()
+        {
+            var db = DataProvider.Ins.DB;
+            var data = db.Products.ToList();
+            var d = (db.OrdersDetails.GroupBy(x => x.ProductID).ToList());
+            var area = new ChartArea();
+            var chartProduct = new Chart();
+            chartProduct.ChartAreas.Add(area);
+            var series = new Series();
+            foreach (var i in d)
+            {
+                series.Points.AddXY(i.Key, i.Select(a => a.Quantity).Sum());
+            }
+            series.ChartType = SeriesChartType.RangeBar;
+            series.Label = "#PERCENT{P2}";
+            series.Font = new Font("Arial", 8.0f, FontStyle.Bold);
+            series.Palette = ChartColorPalette.Light;
+            Title title = new Title("Product selling");
+            title.Font = new Font("Matura MT Script Capitals", 14f, FontStyle.Bold);
+            chartProduct.Titles.Add(title);
+            chartProduct.Series.Add(series);
+            chartProduct.Dock = DockStyle.Fill;
+            panelProduct.Controls.Add(chartProduct);
+        }
+    }
+}
