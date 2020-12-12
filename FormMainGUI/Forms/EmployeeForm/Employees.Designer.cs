27f2e0f243da2f76d0c6211464e017f04bc9282f
GiaKhai <khaigia11111@gmail.com>
2020-12-13 00:27:18 +0700

    fix design employee

--- FormMainGUI/Forms/EmployeeForm/Employees.Designer.cs
+++ FormMainGUI/Forms/EmployeeForm/Employees.Designer.cs
@@ -54,11 +54,12 @@
             // 
             this.groupBox1.Controls.Add(this.dgvEmployee);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.Blue;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1043, 473);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Size = new System.Drawing.Size(1043, 451);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Employee List";
@@ -67,10 +68,10 @@
             // 
             this.dgvEmployee.AllowUserToResizeRows = false;
             this.dgvEmployee.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvEmployee.BackgroundColor = System.Drawing.Color.White;
+            this.dgvEmployee.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dgvEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvEmployee.Dock = System.Windows.Forms.DockStyle.Top;
-            this.dgvEmployee.Location = new System.Drawing.Point(3, 24);
+            this.dgvEmployee.Location = new System.Drawing.Point(10, 33);
             this.dgvEmployee.Margin = new System.Windows.Forms.Padding(5, 3, 3, 3);
             this.dgvEmployee.MultiSelect = false;
             this.dgvEmployee.Name = "dgvEmployee";
@@ -78,7 +79,7 @@
             this.dgvEmployee.RowTemplate.Height = 24;
             this.dgvEmployee.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
             this.dgvEmployee.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvEmployee.Size = new System.Drawing.Size(1037, 443);
+            this.dgvEmployee.Size = new System.Drawing.Size(1023, 405);
             this.dgvEmployee.TabIndex = 1;
             this.dgvEmployee.SelectionChanged += new System.EventHandler(this.dgvEmployee_SelectionChanged);
             // 
@@ -88,9 +89,9 @@
             this.panel2.Controls.Add(this.btnDelete);
             this.panel2.Controls.Add(this.btnUpdate);
             this.panel2.Controls.Add(this.btnAdd);
-            this.panel2.Location = new System.Drawing.Point(0, 479);
+            this.panel2.Location = new System.Drawing.Point(0, 457);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1043, 158);
+            this.panel2.Size = new System.Drawing.Size(1043, 180);
             this.panel2.TabIndex = 1;
             // 
             // btnDelete
@@ -100,7 +101,7 @@
             this.btnDelete.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnDelete.Depth = 0;
             this.btnDelete.Icon = null;
-            this.btnDelete.Location = new System.Drawing.Point(933, 44);
+            this.btnDelete.Location = new System.Drawing.Point(933, 55);
             this.btnDelete.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Primary = true;
@@ -117,7 +118,7 @@
             this.btnUpdate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnUpdate.Depth = 0;
             this.btnUpdate.Icon = null;
-            this.btnUpdate.Location = new System.Drawing.Point(778, 44);
+            this.btnUpdate.Location = new System.Drawing.Point(778, 55);
             this.btnUpdate.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Primary = true;
@@ -134,7 +135,7 @@
             this.btnAdd.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnAdd.Depth = 0;
             this.btnAdd.Icon = null;
-            this.btnAdd.Location = new System.Drawing.Point(609, 44);
+            this.btnAdd.Location = new System.Drawing.Point(609, 55);
             this.btnAdd.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Primary = true;
